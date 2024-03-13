2cd7eb86c92348cbeabacb7e2f13a15bd2026807
bipproduction <bip.production.js@gmail.com>
2024-03-13 20:36:32 +0800

    auto push

--- exp_client.js
+++ exp_client.js
@@ -1,6 +1,7 @@
 const { spawn } = require('child_process')
 const yargs = require('yargs')
 const path = require('path')
+require('colors')
 yargs
     .command(
         "start",
@@ -16,13 +17,20 @@ async function start() {
     const path1 = path.join(process.cwd(), 'apps', 'p2p', 'example', 'client', 'satu')
     const path2 = path.join(process.cwd(), 'apps', 'p2p', 'example', 'client', 'dua')
 
+    const child3 = spawn('/bin/bash', ['-c', `node . start`])
+    child3.stdout.on('data', (data) => {
+        console.log("[p2p]: ".green, data.toString())
+    })
+
     const child1 = spawn('/bin/bash', ['-c', `serve -p 3011 -s ${path1}`])
     child1.stdout.on('data', (data) => {
-        console.log("[client1]: " + data.toString())
+        console.log("[client1]: ".yellow + data.toString())
     })
 
     const child2 = spawn('/bin/bash', ['-c', `serve -p 3012 -s ${path2}`])
     child2.stdout.on('data', (data) => {
-        console.log("[client2]: ", data.toString())
+        console.log("[client2]: ".blue, data.toString())
     })
+
+
 }
